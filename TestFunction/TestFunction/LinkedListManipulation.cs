20d26d6d19b5ae34364bc076a4f7a137afe97ef4
nchejara <pchejara@live.com>
2013-05-23 22:20:32 -0700

    Add TestFunction Porject for practice C# logic

---
+++ TestFunction/TestFunction/LinkedListManipulation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFunction
+{
+    public class Node
+    {
+        public Object data { get; set; }
+        public Node Next { get; set; }
+    }
+
+    public class LinkedListManipulation
+    {
+        private Node Head;
+
+        public void AddNode(Node newNode)
+        {
+            newNode.Next = this.Head;
+            this.Head = newNode;
+        }
+
+        public void printNodes()
+        {
+            while (Head != null)
+            {
+                Console.Write(Head.data + " ");
+                Head = Head.Next;
+            }
+        }
+    }
+
+
+}
---
+++ TestFunction/TestFunction/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(StringManipulation.Reverse("Naren"));
+            //Console.WriteLine(StringManipulation.Trim("      N c\nM p\nKeshav     "));
+            //Console.WriteLine(StringManipulation.LeftTrim("      N c\nM p\nKeshav     "));
+            //Console.WriteLine(StringManipulation.RightTrim("      N c\nM p\nKeshav     "));
+            //Console.WriteLine(StringManipulation.ReverseWordByWord("   I am Naren Chejara\nand you are kumar "));
+            //Console.WriteLine("Total Line : {0} || TotalWord : {1}", StringManipulation.TotalLine("   I am Naren Chejara\nand you are kumar "), StringManipulation.TotalWord("   I am Naren Chejara\nand you are kumar "));
+            //Console.WriteLine(StringManipulation.RemoveAdditionalSpace("   I      am     Naren    Chejara\nand      you     are    kumar "));
+            //Console.WriteLine(StringManipulation.RemoveDuplicateChar("aabbccccccccccccccccc"));
+            //Console.WriteLine(StringManipulation.RemoveDuplicateChar("aaabbbbbcccc"));
+            //[To do]: Below line string not work.need to sort in asending order
+             //Console.WriteLine(StringManipulation.RemoveDuplicateChar("abcabc"));
+
+            // Linked List
+
+            LinkedListManipulation llm = new LinkedListManipulation();
+            Node nodeA = new Node();
+            nodeA.data = "A";
+
+            Node nodeB = new Node();
+            nodeB.data = "B";
+
+            Node nodeC = new Node();
+            nodeC.data = "C";
+
+            llm.AddNode(nodeA);
+            llm.AddNode(nodeB); 
+            llm.AddNode(nodeC);
+
+            llm.printNodes();
+
+            Console.ReadLine();
+        }
+    }
+    
+    
+}
+
